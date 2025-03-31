f3e4c55a41340422489191d705af1bde799f94d7
JulietaJara <112047070+JulietaJara@users.noreply.github.com>
2025-03-31 16:37:03 -0300

    error arreglado

--- check/src/app/components/ui/card.tsx
+++ check/src/app/components/ui/card.tsx
@@ -1,9 +1,7 @@
 import * as React from "react"
 import { cn } from "../../lib/utils"
 
-export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {}
-
-const Card = React.forwardRef<HTMLDivElement, CardProps>(
+const Card = React.forwardRef<HTMLDivElement, React.HTMLProps<HTMLDivElement>>(
   ({ className, children, ...props }, ref) => {
     return (
       <div
@@ -21,19 +19,19 @@ const Card = React.forwardRef<HTMLDivElement, CardProps>(
 )
 Card.displayName = "Card"
 
-const CardHeader = ({ className, children, ...props }: CardProps) => (
+const CardHeader = ({ className, children, ...props }: React.HTMLProps<HTMLDivElement>) => (
   <div className={cn("mb-4 text-lg font-semibold", className)} {...props}>
     {children}
   </div>
 )
 
-const CardContent = ({ className, children, ...props }: CardProps) => (
+const CardContent = ({ className, children, ...props }: React.HTMLProps<HTMLDivElement>) => (
   <div className={cn("text-gray-700", className)} {...props}>
     {children}
   </div>
 )
 
-const CardFooter = ({ className, children, ...props }: CardProps) => (
+const CardFooter = ({ className, children, ...props }: React.HTMLProps<HTMLDivElement>) => (
   <div className={cn("mt-4 border-t pt-4", className)} {...props}>
     {children}
   </div>
